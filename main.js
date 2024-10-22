db954c190870b9c54463fb90be6e582e528d434d
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-22 14:41:18 +0200

    add lamp

--- main.js
+++ main.js
@@ -2,8 +2,24 @@
 //Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 //Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 
+
+//BTN
 const myButtonEl = document.getElementById ("btn-lettering");
+
+
+//IMG
+//genero il nodo dell'img
+const myFigurElement = document.getElementById ("img-container");
+//recupero il tag img
+const myCustumImg = document.createElement ('img');
+myCustumImg.src = "./img/white_lamp.png";
+
+//aggiungo il nodo dell'immagine al nodo figure
+myFigurElement.append(myCustumImg);
+
+//click function
 myButtonEl.addEventListener("click", () =>{
-myButtonEl.innerHTML = "Spegni";
-}
-)
+    myButtonEl.innerHTML= "Spegni";
+    myCustumImg.src ="./img/yellow_lamp.png";
+    }
+    )
